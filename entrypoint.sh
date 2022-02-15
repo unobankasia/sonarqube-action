3cd47b3510a1113145910b3a526ba24eaf3d56a2
Sumit Kadian <sumit.kadian@xebia.com>
2022-02-15 06:37:23 +0000

    changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,7 @@ if [[ ! -f "${GITHUB_WORKSPACE}/sonar-project.properties" ]]; then
     -Dsonar.password=${SONAR_PASSWORD} \
     -Dsonar.sources=. \
     -Dsonar.sourceEncoding=UTF-8
+    -Dsonar.java.binaries=${binaries}
 else
   sonar-scanner \
     -Dsonar.host.url=${INPUT_HOST} \
